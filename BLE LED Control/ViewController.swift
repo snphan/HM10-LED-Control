c3544bd12033f531df5074270b92c0161aeea1cc
Steven Phan <stevenphan@Stevens-Air.hitronhub.home>
2019-07-26 19:08:47 -0600

    Initialize Low Energy Bluetooth project

---
+++ BLE LED Control/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BLE LED Control
+//
+//  Created by Steven Phan on 2019-07-26.
+//  Copyright © 2019 Steven Phan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: VARIABLES
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var disconnectButton: UIButton!
+    @IBOutlet weak var onOffSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
